21a8dcb4dbbfdfadfd61005417476d8f8739ed7d
Abdelrhman <abdelrhman@mac.local>
2022-10-03 18:35:33 +0200

    a

--- lib/core/presentation/router/app_router.dart
+++ lib/core/presentation/router/app_router.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 abstract class AppRouter {
-  static String routeOf({required Widget screen}) {
+  static String routeOf(Widget screen) {
     _routes[screen.toString()] = (context) => screen;
     return screen.toString();
   }
 
-
   static Map<String, WidgetBuilder> _routes = {};
-  
+
   static Map<String, WidgetBuilder> get routes => _routes;
 }
