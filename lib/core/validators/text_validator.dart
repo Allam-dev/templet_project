3b08b2d3da9c317cebe9b46fca14e9c5f1b3c751
Abdelrhman <abdelrhman@mac.local>
2023-08-12 20:07:18 +0300

    a

--- lib/core/validators/text_validator.dart
+++
@@ -1,9 +0,0 @@
-extension on String? {
-  String? get validAsText {
-    if (this == null || this!.isEmpty) {
-      return 'This field is required';
-    } else {
-      return null;
-    }
-  }
-}
